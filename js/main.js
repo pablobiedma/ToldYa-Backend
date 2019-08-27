765783db6edf734c296d6159a1b2f34700639580
Pablo Biedma <P.BiedmaNunez@student.tudelft.nl>
2019-08-27 21:32:50 +0200

    removed validation

--- js/main.js
+++ js/main.js
@@ -18,53 +18,53 @@
             
   
     
-    /*==================================================================
-    [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var message = $('.validate-input textarea[name="message"]');
+    // /*==================================================================
+    // [ Validate ]*/
+    // var name = $('.validate-input input[name="name"]');
+    // var email = $('.validate-input input[name="email"]');
+    // var message = $('.validate-input textarea[name="message"]');
 
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+    // $('.validate-form').on('submit',function(){
+    //     var check = true;
 
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
-        }
-
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
-        }
-
-        return check;
-    });
+    //     if($(name).val().trim() == ''){
+    //         showValidate(name);
+    //         check=false;
+    //     }
 
 
-    $('.validate-form .input2').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
-    });
+    //     if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    //         showValidate(email);
+    //         check=false;
+    //     }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+    //     if($(message).val().trim() == ''){
+    //         showValidate(message);
+    //         check=false;
+    //     }
 
-        $(thisAlert).addClass('alert-validate');
-    }
+    //     return check;
+    // });
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
+    // $('.validate-form .input2').each(function(){
+    //     $(this).focus(function(){
+    //        hideValidate(this);
+    //    });
+    // });
+
+    // function showValidate(input) {
+    //     var thisAlert = $(input).parent();
+
+    //     $(thisAlert).addClass('alert-validate');
+    // }
+
+    // function hideValidate(input) {
+    //     var thisAlert = $(input).parent();
+
+    //     $(thisAlert).removeClass('alert-validate');
+    // }
     
     
 
