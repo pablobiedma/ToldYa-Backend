b7e42f62b705e69bdae18f9e4d6655a0701f6522
inttypes <inttypes.h@gmail.com>
2019-08-27 23:05:58 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bitcore = require('bitcore-lib') // Adding bitcore library
+var bitcore = require('bitcore-lib') // Adding bitcore library
 
 const app = express();
 const path = require('path');
@@ -8,6 +8,32 @@ const router = express.Router();
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.get('/api', function(req, res){ console.log("==== TRANSACTION ====");
+var field1 = req.query.field1;
+var field2 = req.query.field2;
+var field3 = req.query.field3;
+
+console.log(field1);
+console.log(field2);
+console.log(field3);
+
+var privateKey = new bitcore.PrivateKey(field2); //we add our private key here 
+var utxo = {
+  "txId" : "9fca330d7144c2ee45d0e31d6dc79910d33dccf6da6a8e4dae1573da82e77194",
+  "outputIndex" : 0,
+  "address" : field3,  //receiver address (public key)
+  "script" : "76a9147adc5be0fe744e4bbe04d40723fb699bd00c9e3088ac",  
+  "satoshis" : 50000
+};
+
+var transaction = new bitcore.Transaction()
+    .from(utxo)
+    .addData(field1) // Add OP_RETURN data
+    .sign(privateKey);
+
+console.log(transaction);
+});//res.send('Hello World!'))
 
 //add the router
 app.use('/', router);
