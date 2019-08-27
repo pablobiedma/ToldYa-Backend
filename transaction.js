6c2d2e35cd2b8406b18174b94f6b40355e0f6e11
Pablo Biedma <P.BiedmaNunez@student.tudelft.nl>
2019-08-27 18:37:20 +0200

    important changes

--- transaction.js
+++ transaction.js
@@ -1,17 +1,26 @@
-var bitcore = require("bitcore-lib") // Adding bitcore library
+//var bitcore = require("bitcore-lib") // Adding bitcore library
+var submit = document.getElementById("submit");
+//var privateKey = new bitcore.PrivateKey(secret); //we add our private key here 
 
-	// We can generate an address at  bitaddress.org
-		
-var privateKey = new bitcore.PrivateKey(''); //we add our private key here 
-var utxo = {
-  "txId" : "",
-  "outputIndex" : 0,
-  "address" : "",  //receiver address (public key)
-  "script" : "",  
-  "satoshis" : 50000
-};
+submit.onclick = function () {
+    var message = document.getElementById("message").value;
+    var secret = document.getElementById("private").value;
+    var public = document.getElementById("public").value;
+    console.log(message);
+    console.log(secret);
+    console.log(public);
+}
 
-var transaction = new bitcore.Transaction()
-    .from(utxo)
-    .addData('told ya') // Add OP_RETURN data
-    .sign(privateKey);
+
+//var utxo = {
+//  "txId" : "", //waiting for maximme
+//    "outputIndex": 0, //waiting for maximme
+//    "address": public,  
+//    "script": "00140a19cc70549b02a9e6d9e496e7ecbfbc54026367",  //waiting for maximme
+//  "satoshis" : 50000
+//};
+
+//var transaction = new bitcore.Transaction()
+//    .from(utxo)
+//    .addData(message) // Add OP_RETURN data
+//    .sign(privateKey);
