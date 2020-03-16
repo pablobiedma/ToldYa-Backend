c68f8f012d72df139171de987cea5c83c92c8be8
Pablo Biedma <P.BiedmaNunez@student.tudelft.nl>
2020-03-16 15:08:08 +0100

    version 2 (no webpage)

--- server.js
+++ server.js
@@ -1,59 +1,14 @@
-const express = require('express');
-var bitcore = require('bitcore-lib') // Adding bitcore library
-var litecore = require("litecore-lib") // Adding litecore library
+const express = require("express");
+const bodyParser = require("body-parser");
+const rpcMethods = require("./routes/api");
 
 const app = express();
-const path = require('path');
-const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/api', function(req, res){ console.log("==== TRANSACTION ====");
-var message = req.query.message;
-var private = req.query.private;
-var crypto = req.query.crypto;
-console.log(private);
-console.log(public);
-console.log(message);
+app.use("/api", rpcMethods);
 
-if(crypto == "btc") {
-	var privateKey = new bitcore.PrivateKey(private); //we add our private key here 
-	var utxo = {
-	  "txId" : "9fca330d7144c2ee45d0e31d6dc79910d33dccf6da6a8e4dae1573da82e77194",
-	  "outputIndex" : 0,
-	  "address" : "1CCdRD5VJ9KsCF88dBNF71fQuihS2s1PDU",  //receiver address (public key)
-	  "script" : "76a9147adc5be0fe744e4bbe04d40723fb699bd00c9e3088ac",  
-	  "satoshis" : 100000
-	};
+const port = process.env.PORT || 4444;
 
-	var transaction = new bitcore.Transaction()
-	    .from(utxo)
-	    .addData(message) // Add OP_RETURN data
-	    .sign(privateKey);
-
-	console.log(transaction);
-} else if(crypto == "ltc") {
-	// Variables
-	var privateKey = new litecore.PrivateKey(private);
-	var utxo = {
-	  "txId" : "",
-	  "outputIndex" : 0,
-	  "address" : public,
-	  "script" : "76a91447862fe165e6121af80d5dde1ecb478ed170565b88ac",
-	  "satoshis" : 100000
-	};
-
-	var transaction = new litecore.Transaction()
-	    .from(utxo)
-	    .addData(message) // Add OP_RETURN data
-	    .sign(privateKey);
-}
-});//res.send('Hello World!'))
-
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 1337);
-
-console.log('Running at Port 1337');
+server = app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ wallet.js
@@ -0,0 +1,4 @@
+module.exports = {
+	"address": "2N5aWLahighZA4EVJUcmFYM528N4sk92EsX",
+	"privateKey": "cVfq5uusH7HLfqaENZMmDr2LVq1tYyusS5EKXvK1MDSo6VDX5eku"
+}
